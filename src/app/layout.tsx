14c5762a72781b3d313708e53c7a009d7ea18c4e
Thiago Pereira <pereira.tp.thiago@gmail.com>
2025-10-18 16:30:11 -0300

    build(deps): adicionado e configurado tailwind e shadcn-ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -11,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true} data-lt-installed={true}>
       <body>{children}</body>
     </html>
   );
